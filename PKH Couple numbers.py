1a9abc5f214e2c23bfbeea1ea99b0cf4d037f986
Iliyana Mircheva <mircheva.iliana@gmail.com>
2023-03-07 10:35:38 +0200

    PKH Couple numbers refactor

--- PKH Couple numbers.py
+++ PKH Couple numbers.py
@@ -6,16 +6,15 @@
 
 def my_func(l, n):
     result = []
-    start = l[0]
-    end = l[-1]
+    my_set = set(l)
+
     for item in l:
-        if start <= n - item <= end:
-            if n - item != item:  # eliminate adding the same number twice
-                if item < n // 2 + 1:  # eliminate repeating couples
-                    result.append([item, n - item])
+        wanted = n - item
+        if wanted in my_set and wanted < n / 2:
+            result.append([item, wanted])
     return tuple(result)
 
 
-ll = my_func([1, 2, 3, 4, 5, 6, 7, 8, 9], 17)
+ll = my_func([10, 20, 30, 40, 50, 60, 70, 80], 40)
 print(ll)
 
